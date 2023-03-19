13e8191c34b454757085686b60d09e0311319ef6
adiv031198 <73923291+adiv031198@users.noreply.github.com>
2023-03-19 18:36:58 +0800

    Last BufferPool Update Inshallah

--- src/java/simpledb/storage/BufferPool.java
+++ src/java/simpledb/storage/BufferPool.java
@@ -159,7 +159,7 @@ public class BufferPool {
         // some code goes here
         // not necessary for 
         HeapFile table = (HeapFile) Database.getCatalog().getDatabaseFile(tableId);
-        ArrayList<Page> affectedPages = table.insertTuple(tid, t);
+        List<Page> affectedPages = table.insertTuple(tid, t);
         for (Page page : affectedPages) {
             page.markDirty(true, tid);
         }
@@ -185,8 +185,10 @@ public class BufferPool {
         // not necessary for lab1
         int tableId=t.getRecordId().getPageId().getTableId();
         HeapFile table = (HeapFile) Database.getCatalog().getDatabaseFile(tableId);
-        Page affectedPage = table.deleteTuple(tid, t);
-        affectedPage.markDirty(true,tid);
+        ArrayList<Page> affectedPage = table.deleteTuple(tid, t);
+        for (Page page : affectedPage) {
+            page.markDirty(true, tid);
+        }
     
     }
     /**
